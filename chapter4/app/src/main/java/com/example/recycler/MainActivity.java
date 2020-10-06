127a6699cddf195f4504582e18ea4a8221f7b03e
zhihuistar <2602528145@qq.com>
2020-10-06 22:59:20 +0800

    修改getRandomLengthName()

--- chapter4/app/src/main/java/com/example/recycler/MainActivity.java
+++ chapter4/app/src/main/java/com/example/recycler/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         Random random = new Random();
         int length = random.nextInt(20) + 1;
         StringBuilder builder = new StringBuilder();
+        builder.append(name);
         for(int i = 0; i < length; i++){
             builder.append("*");
         }
