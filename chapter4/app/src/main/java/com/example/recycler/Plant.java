86edb95850244a01d9e87a43d6a4b1a210406b1c
zhihuistar <2602528145@qq.com>
2020-10-06 23:31:25 +0800

    添加EditText

--- chapter4/app/src/main/java/com/example/recycler/Plant.java
+++ chapter4/app/src/main/java/com/example/recycler/Plant.java
@@ -3,14 +3,19 @@ package com.example.recycler;
 public class Plant {
     private String name;
     private int imageId;
-    public Plant(String name, int imageId){
+    private String function;
+    public Plant(String name, int imageId, String function){
         this.name = name;
         this.imageId = imageId;
+        this.function = function;
     }
     public String getName(){
         return name;
     }
     public int getImageId(){
         return imageId;
+    }
+    public String getFunction(){
+        return function;
     }
 }
