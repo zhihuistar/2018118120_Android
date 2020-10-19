7cbbdab6b497e10d7416a9fc32818dda6208c432
zhihuistar <2602528145@qq.com>
2020-10-19 17:21:54 +0800

    重新创建AnotherBroadcastReceiver

--- chapter5/BroadcastTest2/app/src/main/java/com/example/broadcasttest2/AnotherBroadcastReceiver.java
+++ chapter5/BroadcastTest2/app/src/main/java/com/example/broadcasttest2/AnotherBroadcastReceiver.java
@@ -1,14 +1,12 @@
 package com.example.broadcasttest2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.widget.Toast;
 
 public class AnotherBroadcastReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "received in AnotherBroadcastReceiver", Toast.LENGTH_SHORT).show();
