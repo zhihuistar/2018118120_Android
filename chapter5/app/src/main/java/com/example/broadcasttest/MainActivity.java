3ddc498bfa4ce2db3082a3842661fcf069fabc5e
zhihuistar <2602528145@qq.com>
2020-10-19 16:44:07 +0800

    增补一行代码

--- chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
     }
     protected void onDestroy(){
@@ -26,7 +29,14 @@ public class MainActivity extends AppCompatActivity {
     }
     class NetworkChangeReceiver extends BroadcastReceiver{
         public void onReceive(Context context, Intent intent){
-            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+            ConnectivityManager connectionManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+            if(networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
