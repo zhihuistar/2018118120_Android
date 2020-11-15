6171b76f35b327e436844b7c317fd1bf6aecdcc0
zhihuistar <2602528145@qq.com>
2020-11-15 21:35:04 +0800

    新建MybroadcastReceiver

--- chapter5/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
+++ chapter5/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
@@ -10,6 +10,5 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
-        abortBroadcast();
     }
 }
