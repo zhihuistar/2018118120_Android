1290e4d2ac57ce1d13cd684621cacce12f7a8247
zhihuistar <2602528145@qq.com>
2020-11-15 20:58:50 +0800

    再次更新MainActivity

--- chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
@@ -32,7 +33,13 @@ public class MainActivity extends AppCompatActivity {
     }
     class NetworkChangeReceiver extends BroadcastReceiver{
         public void onReceive(Context context, Intent intent){
-            Toast.makeText(context,"network changes",Toast.LENGTH_SHORT).show();
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if(networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"network is unavailable",Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
