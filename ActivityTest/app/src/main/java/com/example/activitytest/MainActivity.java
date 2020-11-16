197276f77e229c8ce6e2ec94e7cf3ce05ceddf47
zhihuistar <2602528145@qq.com>
2020-11-16 14:23:12 +0800

    为MainActivity重写onActivityResult()方法

--- ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -23,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,1);
             }
         });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
