998198c5349580d7d0cb2595fd1ec672cc0af9f8
zhihuistar <2602528145@qq.com>
2020-11-16 13:22:08 +0800

    修改MainActivity

--- ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.activitytest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent("com.example.activitytest.ACTION_START");
-                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
                 startActivity(intent);
             }
         });
