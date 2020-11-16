1b02502b8a8b606023e65576668602c7c2042592
zhihuistar <2602528145@qq.com>
2020-11-16 12:59:15 +0800

    再次修改MainActivity类

--- ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.activitytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
