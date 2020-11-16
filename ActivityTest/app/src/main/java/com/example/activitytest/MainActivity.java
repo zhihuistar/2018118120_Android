b781a7f1130ca2bb0b8748e9fbc6258dd9cbbb95
zhihuistar <2602528145@qq.com>
2020-11-16 14:00:52 +0800

    修改MainActivity

--- ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.baidu.com"));
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
                 startActivity(intent);
             }
         });
