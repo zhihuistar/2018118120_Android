db49324d562f3d99888e7f5bbe2d722eb58eae62
zhihuistar <2602528145@qq.com>
2020-11-16 14:07:36 +0800

    完善SecondActivity

--- ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.activitytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity",data);
     }
 }
