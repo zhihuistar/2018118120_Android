b6b329848fd8461e0f0b05f2416444938bf8b8da
zhihuistar <2602528145@qq.com>
2020-12-04 10:23:31 +0800

    设置MainActivity类

--- chapter12/app/src/main/java/com/example/materialtest/MainActivity.java
+++ chapter12/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.materialtest;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
