4f1082a3d36aa4f0de355673d6341c5cc08ee0c2
zhihuistar <2602528145@qq.com>
2020-12-13 19:06:50 +0800

    修改MainActivity

--- chapter8/app/src/main/java/com/example/chapter8/MainActivity.java
+++ chapter8/app/src/main/java/com/example/chapter8/MainActivity.java
@@ -10,42 +10,35 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     private TextView text;
-
     private Handler handler = new Handler(){
         public void handleMessage(Message msg){
             text.setText(msg.obj.toString());
         }
     };
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.setTitle("多线程");
-
+        this.setTitle("线程之间");
         text = (TextView)findViewById(R.id.textView);
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(this);
     }
-
     public void onClick(View v){
         switch (v.getId()){
             case R.id.button:
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        int count = 0;
+                        int n = 0;
                         while(true){
-                            count = count + 1;
+                            n++;
                             Message message = new Message();
-                            String string = "线程号 : " + Thread.currentThread().getId() + "\n完成的任务数 : " + count;
+                            String string = "线程号 : " + Thread.currentThread().getId() + "\n完成的任务数 : " + n;
                             message.obj = string;
                             handler.sendMessage(message);
                             try {
-                                Thread.sleep(5000);
+                                Thread.sleep(3000);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
