70c7078518565cb32325fca7e281b3dde828d530
zhihuistar <2602528145@qq.com>
2020-12-13 21:18:26 +0800

    修改MainActivity

--- chapter10/app/src/main/java/com/example/chapter10/MainActivity.java
+++ chapter10/app/src/main/java/com/example/chapter10/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             super.onProgressUpdate(values);
         }
         protected void onPostExecute(Void aVoid) {
+            MainActivity.textView.setText("服务停止了！");
             super.onPostExecute(aVoid);
         }
     }
