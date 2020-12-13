1ba9011cdc5e48054596a97091bfb0e470057678
zhihuistar <2602528145@qq.com>
2020-12-13 20:15:50 +0800

    修改MainActivity

--- chapter9/app/src/main/java/com/example/chapter9/MainActivity.java
+++ chapter9/app/src/main/java/com/example/chapter9/MainActivity.java
@@ -15,14 +15,9 @@ public class MainActivity extends AppCompatActivity {
     TextView text;
     ProgressBar progressBar;
     private class MyTask extends AsyncTask<String, Integer, String> {
-        // 方法1：onPreExecute（）
-        // 作用：执行 线程任务前的操作
         protected void onPreExecute() {
             text.setText("加载中");
         }
-        // 方法2：doInBackground（）
-        // 作用：接收输入参数、执行任务中的耗时操作、返回 线程任务执行的结果
-        // 此处通过计算从而模拟“加载进度”的情况
         protected String doInBackground(String... params) {
             try {
                 int count = 0;
@@ -36,19 +31,13 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
-        // 方法3：onProgressUpdate（）
-        // 作用：在主线程 显示线程任务执行的进度
         protected void onProgressUpdate(Integer... progresses) {
             progressBar.setProgress(progresses[0]);
             text.setText("loading..." + progresses[0] + "%");
         }
-        // 方法4：onPostExecute（）
-        // 作用：接收线程任务执行结果、将执行结果显示到UI组件
         protected void onPostExecute(String result) {
             text.setText("加载完毕");
         }
-        // 方法5：onCancelled()
-        // 作用：将异步任务设置为：取消状态
         protected void onCancelled() {
             text.setText("已取消");
             progressBar.setProgress(0);
