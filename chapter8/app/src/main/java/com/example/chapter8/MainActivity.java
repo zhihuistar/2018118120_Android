a0bc1f63efeb08d8ef43a387a700c4957754d0f9
zhihuistar <2602528145@qq.com>
2020-12-13 18:08:05 +0800

    编写MainActivity

---
+++ chapter8/app/src/main/java/com/example/chapter8/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.chapter8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView text;
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            text.setText(msg.obj.toString());
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.setTitle("多线程");
+
+        text = (TextView)findViewById(R.id.textView);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int count = 0;
+                        while(true){
+                            count = count + 1;
+                            Message message = new Message();
+                            String string = "线程号 : " + Thread.currentThread().getId() + "\n完成的任务数 : " + count;
+                            message.obj = string;
+                            handler.sendMessage(message);
+                            try {
+                                Thread.sleep(5000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+}
