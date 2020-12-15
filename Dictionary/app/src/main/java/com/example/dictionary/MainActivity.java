fc9931617f052c5dd5a49371ff8373394fc6290b
zhihuistar <2602528145@qq.com>
2020-12-15 23:44:56 +0800

    在com.example.dictionary中新建一个utils包

--- Dictionary/app/src/main/java/com/example/dictionary/MainActivity.java
+++ Dictionary/app/src/main/java/com/example/dictionary/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView pyTv,bsTv,cyuTv,twenTv,juziTv;
+    EditText ziEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+    private void initView() {
+        pyTv = findViewById(R.id.main_tv_pinyin);
+        bsTv = findViewById(R.id.main_tv_bushou);
+        cyuTv = findViewById(R.id.main_tv_chengyu);
+        twenTv = findViewById(R.id.main_tv_tuwen);
+        juziTv = findViewById(R.id.main_tv_juzi);
+        ziEt = findViewById(R.id.main_et);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            case R.id.main_iv_setting:
+                break;
+            case R.id.main_iv_search:
+                break;
+            case R.id.main_tv_pinyin:
+                break;
+            case R.id.main_tv_bushou:
+                break;
+            case R.id.main_tv_chengyu:
+                break;
+            case R.id.main_tv_tuwen:
+                break;
+        }
     }
 }
