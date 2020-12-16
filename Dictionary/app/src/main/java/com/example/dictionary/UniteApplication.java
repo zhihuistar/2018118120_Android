52e051b855e6d76f4e62f34eb4166c8d64cdadd9
zhihuistar <2602528145@qq.com>
2020-12-16 23:24:08 +0800

    在UniteApplication类中初始化xUtils的模块数据

--- Dictionary/app/src/main/java/com/example/dictionary/UniteApplication.java
+++ Dictionary/app/src/main/java/com/example/dictionary/UniteApplication.java
@@ -2,5 +2,11 @@ package com.example.dictionary;
 
 import android.app.Application;
 
+import org.xutils.x;
+
 public class UniteApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);   //初始化xUtils的模块数据
+    }
 }
