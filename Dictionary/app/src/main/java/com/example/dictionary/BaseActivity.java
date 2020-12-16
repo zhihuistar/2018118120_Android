7c7584547dc40130096ebef39a9af1523b448712
zhihuistar <2602528145@qq.com>
2020-12-16 23:14:48 +0800

    也让BaseActivity类实现Callback.CommonCallback接口

--- Dictionary/app/src/main/java/com/example/dictionary/BaseActivity.java
+++ Dictionary/app/src/main/java/com/example/dictionary/BaseActivity.java
@@ -2,5 +2,26 @@ package com.example.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+import org.xutils.common.Callback;
+
+public class BaseActivity extends AppCompatActivity implements Callback.CommonCallback {
+    @Override
+    public void onSuccess(Object result) {
+
+    }
+
+    @Override
+    public void onError(Throwable ex, boolean isOnCallback) {
+
+    }
+
+    @Override
+    public void onCancelled(CancelledException cex) {
+
+    }
+
+    @Override
+    public void onFinished() {
+
+    }
 }
