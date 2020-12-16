edecfc006974190a7d04012ed7c6358e5eaf5137
zhihuistar <2602528145@qq.com>
2020-12-16 21:59:51 +0800

    在CommonUtils类中定义“选中拼音或部首”和“拼音和部首的txt”的常量

--- Dictionary/app/src/main/java/com/example/dictionary/utils/CommonUtils.java
+++ Dictionary/app/src/main/java/com/example/dictionary/utils/CommonUtils.java
@@ -1,4 +1,8 @@
 package com.example.dictionary.utils;
 
 public class CommonUtils {
+    public static final int TYPE_PINYIN = 0;
+    public static final int TYPE_BUSHOU = 1;
+    public static final String FILE_PINYIN = "pinyin.txt";
+    public static final String FILE_BUSHOU = "bushou.txt";
 }
