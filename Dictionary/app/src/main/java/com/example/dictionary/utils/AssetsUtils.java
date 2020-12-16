6e4994413be5d2ba787fe0f33071845e87665e3b
zhihuistar <2602528145@qq.com>
2020-12-16 14:11:01 +0800

    编写AssetsUtils类

--- Dictionary/app/src/main/java/com/example/dictionary/utils/AssetsUtils.java
+++ Dictionary/app/src/main/java/com/example/dictionary/utils/AssetsUtils.java
@@ -1,4 +1,32 @@
 package com.example.dictionary.utils;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+//读取Assets文件夹下内容的工具类
 public class AssetsUtils {
+    public static String getAssetsContent(Context context, String filename){
+        AssetManager manager = context.getResources().getAssets();  //获取Assets文件夹管理者对象
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            InputStream is = manager.open(filename);
+            int hasRead = 0;
+            byte[] buf = new byte[1024];
+            while (true){
+                hasRead = is.read(buf);
+                if (hasRead==-1) {
+                    break;
+                }else{
+                    baos.write(buf,0,hasRead);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return baos.toString();
+
+    }
 }
