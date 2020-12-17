102ee338f890dad6498275cb5ff0c9fda89154ac
zhihuistar <2602528145@qq.com>
2020-12-17 10:44:31 +0800

    在com.example.dictionary下新建一个db包，在包内新建一个DBOpenHelper类，继承SQLiteOpenHelper类

---
+++ Dictionary/app/src/main/java/com/example/dictionary/db/DBOpenHelper.java
@@ -0,0 +1,23 @@
+package com.example.dictionary.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+    public DBOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
