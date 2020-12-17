0dd72af5da7dfb3d217e52b976c87a262d9a42cc
zhihuistar <2602528145@qq.com>
2020-12-17 13:25:00 +0800

    在WordBean类用“汉字查询”的json再次GsonFormat获得“文字详情”的数据源

--- Dictionary/app/src/main/java/com/example/dictionary/bean/WordBean.java
+++ Dictionary/app/src/main/java/com/example/dictionary/bean/WordBean.java
@@ -1,4 +1,136 @@
 package com.example.dictionary.bean;
 
+import java.util.List;
+
 public class WordBean {
+
+    /**
+     * reason : 返回成功
+     * result : {"id":"3dae6b3f20b94a07","zi":"谢","py":"xie","wubi":"ytmf","pinyin":"xiè","bushou":"讠","bihua":"12","jijie":["谢","（謝）","xiè","对别人的帮助或赠与表示感激：谢谢。谢仪。谢忱（谢意）。谢恩。谢意。面谢。致谢。感谢。","认错，道歉：谢过。谢罪。","推辞：谢绝。闭门谢客。","凋落，衰退：谢顶。谢世（去世）。凋谢。新陈代谢。","告诉，询问：\u201c使人称谢：\u2018皇帝敬劳将军。\u2019\u201d","逊，不如：\u201c锦江何谢曲江池？\u201d","古同\u201c榭\u201d，建在台上的房屋。","姓。","开","","笔画数：12；","部首：讠；","笔顺编号：453251113124"],"xiangjie":["谢","謝","xiè","【动】","(形声。从言,射声。本义:向人认错道歉)","同本义〖excuseoneself;makeanapologize〗","自谢曰。\u2014\u2014《战国策·赵策》","长跪而谢。\u2014\u2014《战国策·魏策》","旦日不可不蚤自来谢项王。\u2014\u2014《史记·项羽本纪》","惊谢曰。\u2014\u2014清·周容《芋老人传》","又如:谢咎(引咎自退;悔过;谢罪);谢错(认错赔不是);谢室(待罪之室)","用言辞委婉地推辞拒绝〖decline;refuse〗","乃召拜黯为淮阳太守,黯伏谢不受命。\u2014\u2014《史记·汲黯列传》","谢媒人。\u2014\u2014《玉台新咏·古诗为焦仲卿妻作》","楚而谢。\u2014\u2014唐·李朝威《柳毅传》","谢客矣。\u2014\u2014明·宗臣《报刘一丈书》","子退而休之,谢医却药。\u2014\u2014宋·苏轼《盖公堂记》","又如:谢医(谢绝医疗);谢金(谢绝礼金)","辞去官职〖resign〗","若此儿不救,便当回舟谢职,无复游宦之兴矣。\u2014\u2014《南齐书·王僧虔传》","当亦谢官去,岂令心事违。\u2014\u2014唐·王维《送张五归山》","又如:谢老(告老还乡);谢事(卸去官职);谢政(致仕退休);谢役(辞去差使);谢职(卸去官职)","告辞;告别〖takeleave;leave〗","广不谢大将军而起行。\u2014\u2014《史记·李将军列传》","侯生视公子色终不变,乃谢客就车。\u2014\u2014《史记·魏公子列传》","故不复谢。","不报谢?","过谢侯生。","往昔初阳岁,谢家来贵门。\u2014\u2014《古诗为焦仲卿妻作》","又如:谢了扰(告辞);谢别(告别,辞别)","告诉,告诫〖tell;warn〗","谢汉使。\u2014\u2014《汉书·李广苏建传》","多谢后世人,戒之慎勿忘。\u2014\u2014《玉台新咏·古诗为焦仲卿妻作》","以言辞相问候〖sendone'srespectsto〗","至府,为我多谢问赵君。\u2014\u2014《史记·赵广汉传》","界上亭长寄声谢我,何以不为致问?","感谢〖thank〗","休谢之。\u2014\u2014《三国演义》","哙拜谢,起,立而饮之。\u2014\u2014《史记·项羽本纪》","且谢曰。\u2014\u2014唐·李朝威《柳毅传》","虽死必谢。","又如:谢承(感谢;答谢);谢荷(感谢);谢候(致谢并问候);谢私(感谢私下帮助)","逝去〖farewell〗","诏曰:弟勰所生母潘早龄谢世,显号未加。\u2014\u2014《魏书·彭城王勰传》","又如:谢宾客(死亡的委婉说法);谢世","酬谢〖thanksb.withagift〗","婚礼不许多用盘盒猪羊糖饼,不许厚礼物谢媒。\u2014\u2014明·海瑞《禁约》","受人十万,谢客数千,又重馈部吏。\u2014\u2014《潜夫论》","又如:谢土(修建完毕,祭祀土神的礼义活动);谢犒(酬谢;犒劳);谢银(酬谢用的银子);谢灶诰(祭灶王爷的青词)","衰败,衰落〖decline;wane〗","宿觉名未谢,残山今尚存。\u2014\u2014宋·叶适《宿觉庵》","花之既谢。\u2014\u2014明·李渔《闲情偶寄·种植部》","逊让;不如〖modestlydecline〗","既鲸鲵折首,西夏底定,便宜诉其本怀,避贤谢拙。\u2014\u2014《宋书·王宏传》","又如:谢拙(自谦愚笨而辞职)","避免,避开〖avoid〗","必以俪辞为文,何缘《十翼》不能一致,岂波澜既尽有所谢短乎?\u2014\u2014章炳麟《文学总略》","又如:谢短(避其所短)","","谢","謝","Xiè","【名】","古邑名〖Xietown〗。在今河南省唐河南","于邑于谢,南国是式。\u2014\u2014《大雅》","姓。如:谢女(指晋朝女诗人谢道韫);谢太博(即晋朝谢安;又被称为谢公,谢郎);谢家(指谢安家或指南朝宋的谢灵运家)","","谢病","xièbìng","〖excuseoneselfongroundsofillness〗托病谢绝会客或自请辞职","常为谢病。\u2014\u2014《史记·春申君列传》","应侯(范雎)因谢病。\u2014\u2014《国策·秦策》","谢忱","xièchén","〖gratitude;thanksfulness〗感谢的忱意","承蒙协助,谨致谢忱","谢词","xiècí","〖thank-youspeech〗在正式场合作的表达谢意之辞","谢顶","xièdǐng","〖bebalding〗歇顶","有的老年人谢顶,有的老年人不谢顶","谢恩","xiè\u2019ēn","〖thanksb.(esp.theemperororahighofficial)forhisfavor(orkindness)〗感谢别人的恩德(多指臣子君主)","谢候","xièhòu","〖expressappreciation(forsb.\u2019skindnessorhospitality)〗答报致谢并加问候的意思","谢绝","xièjué","〖decline〗婉辞,推辞","谢绝回答有关个人的问题","谢绝一项正式的邀请","谢客","xièkè","〖refusetomeetvisitorsorguests〗∶婉言拒绝会见宾客","〖expressthankstoguests〗∶向宾客致谢","谢礼","xièlǐ","〖honoravium〗向人致谢送的礼物。也叫\u201c谢仪\u201d","谢灵运","XièLíngyùn","〖XieLingyun〗(385\u2014433)南朝宋诗人,曾任永嘉(现在浙江省永嘉县)太守,喜欢游览,擅长写山水诗,有《谢康乐集》","谢幕","xièmù","〖answeracurtaincall〗演出结束后,演员在台前向观众敬礼,表示谢意","谢媒","xièméi","〖(ofbrideandbridegroom)expresstheirthankstothematchmaker〗举行婚礼后男女双方向媒人致谢","谢却","xièquè","〖decline〗谢绝;婉言拒绝","谢世","xièshì","〖passaway;die〗逝世","谢天谢地","xiètiān-xièdì","〖thankgoodness,thankheaven〗迷信说法,谓感谢天地神灵的佑护。后多用来表示满意或感激之意","谢帖","xiètiě","〖anoteofthanks〗旧时受礼后道谢的回帖","谢孝","xièxiào","〖visitandthankrelativesandfriendsfortheirpresenceatfuneral〗旧俗,孝子到吊唁的亲友家行礼致谢,特指服满后拜访吊唁的亲友,表示感谢","谢谢","xièxie","〖thank〗感谢的表示。感谢,尤指对接受别人给予的或提供的恩惠说些感激的话","谢仪","xièyí","〖honorarium〗谢礼。对一般按习惯或规矩不索取固定报酬的服务或在法律上未规定付酬的服务给予的款项或报酬","谢意","xièyì","〖credit〗感谢之意","谢罪","xièzuì","〖apologizeforanoffence〗自认有过,请人谅解","至蔺相如门谢罪。\u2014\u2014《史记·廉颇蔺相如列传》"]}
+     * error_code : 0
+     */
+
+    private String reason;
+    private ResultBean result;
+    private int error_code;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
+    public static class ResultBean {
+        /**
+         * id : 3dae6b3f20b94a07
+         * zi : 谢
+         * py : xie
+         * wubi : ytmf
+         * pinyin : xiè
+         * bushou : 讠
+         * bihua : 12
+         * jijie : ["谢","（謝）","xiè","对别人的帮助或赠与表示感激：谢谢。谢仪。谢忱（谢意）。谢恩。谢意。面谢。致谢。感谢。","认错，道歉：谢过。谢罪。","推辞：谢绝。闭门谢客。","凋落，衰退：谢顶。谢世（去世）。凋谢。新陈代谢。","告诉，询问：\u201c使人称谢：\u2018皇帝敬劳将军。\u2019\u201d","逊，不如：\u201c锦江何谢曲江池？\u201d","古同\u201c榭\u201d，建在台上的房屋。","姓。","开","","笔画数：12；","部首：讠；","笔顺编号：453251113124"]
+         * xiangjie : ["谢","謝","xiè","【动】","(形声。从言,射声。本义:向人认错道歉)","同本义〖excuseoneself;makeanapologize〗","自谢曰。\u2014\u2014《战国策·赵策》","长跪而谢。\u2014\u2014《战国策·魏策》","旦日不可不蚤自来谢项王。\u2014\u2014《史记·项羽本纪》","惊谢曰。\u2014\u2014清·周容《芋老人传》","又如:谢咎(引咎自退;悔过;谢罪);谢错(认错赔不是);谢室(待罪之室)","用言辞委婉地推辞拒绝〖decline;refuse〗","乃召拜黯为淮阳太守,黯伏谢不受命。\u2014\u2014《史记·汲黯列传》","谢媒人。\u2014\u2014《玉台新咏·古诗为焦仲卿妻作》","楚而谢。\u2014\u2014唐·李朝威《柳毅传》","谢客矣。\u2014\u2014明·宗臣《报刘一丈书》","子退而休之,谢医却药。\u2014\u2014宋·苏轼《盖公堂记》","又如:谢医(谢绝医疗);谢金(谢绝礼金)","辞去官职〖resign〗","若此儿不救,便当回舟谢职,无复游宦之兴矣。\u2014\u2014《南齐书·王僧虔传》","当亦谢官去,岂令心事违。\u2014\u2014唐·王维《送张五归山》","又如:谢老(告老还乡);谢事(卸去官职);谢政(致仕退休);谢役(辞去差使);谢职(卸去官职)","告辞;告别〖takeleave;leave〗","广不谢大将军而起行。\u2014\u2014《史记·李将军列传》","侯生视公子色终不变,乃谢客就车。\u2014\u2014《史记·魏公子列传》","故不复谢。","不报谢?","过谢侯生。","往昔初阳岁,谢家来贵门。\u2014\u2014《古诗为焦仲卿妻作》","又如:谢了扰(告辞);谢别(告别,辞别)","告诉,告诫〖tell;warn〗","谢汉使。\u2014\u2014《汉书·李广苏建传》","多谢后世人,戒之慎勿忘。\u2014\u2014《玉台新咏·古诗为焦仲卿妻作》","以言辞相问候〖sendone'srespectsto〗","至府,为我多谢问赵君。\u2014\u2014《史记·赵广汉传》","界上亭长寄声谢我,何以不为致问?","感谢〖thank〗","休谢之。\u2014\u2014《三国演义》","哙拜谢,起,立而饮之。\u2014\u2014《史记·项羽本纪》","且谢曰。\u2014\u2014唐·李朝威《柳毅传》","虽死必谢。","又如:谢承(感谢;答谢);谢荷(感谢);谢候(致谢并问候);谢私(感谢私下帮助)","逝去〖farewell〗","诏曰:弟勰所生母潘早龄谢世,显号未加。\u2014\u2014《魏书·彭城王勰传》","又如:谢宾客(死亡的委婉说法);谢世","酬谢〖thanksb.withagift〗","婚礼不许多用盘盒猪羊糖饼,不许厚礼物谢媒。\u2014\u2014明·海瑞《禁约》","受人十万,谢客数千,又重馈部吏。\u2014\u2014《潜夫论》","又如:谢土(修建完毕,祭祀土神的礼义活动);谢犒(酬谢;犒劳);谢银(酬谢用的银子);谢灶诰(祭灶王爷的青词)","衰败,衰落〖decline;wane〗","宿觉名未谢,残山今尚存。\u2014\u2014宋·叶适《宿觉庵》","花之既谢。\u2014\u2014明·李渔《闲情偶寄·种植部》","逊让;不如〖modestlydecline〗","既鲸鲵折首,西夏底定,便宜诉其本怀,避贤谢拙。\u2014\u2014《宋书·王宏传》","又如:谢拙(自谦愚笨而辞职)","避免,避开〖avoid〗","必以俪辞为文,何缘《十翼》不能一致,岂波澜既尽有所谢短乎?\u2014\u2014章炳麟《文学总略》","又如:谢短(避其所短)","","谢","謝","Xiè","【名】","古邑名〖Xietown〗。在今河南省唐河南","于邑于谢,南国是式。\u2014\u2014《大雅》","姓。如:谢女(指晋朝女诗人谢道韫);谢太博(即晋朝谢安;又被称为谢公,谢郎);谢家(指谢安家或指南朝宋的谢灵运家)","","谢病","xièbìng","〖excuseoneselfongroundsofillness〗托病谢绝会客或自请辞职","常为谢病。\u2014\u2014《史记·春申君列传》","应侯(范雎)因谢病。\u2014\u2014《国策·秦策》","谢忱","xièchén","〖gratitude;thanksfulness〗感谢的忱意","承蒙协助,谨致谢忱","谢词","xiècí","〖thank-youspeech〗在正式场合作的表达谢意之辞","谢顶","xièdǐng","〖bebalding〗歇顶","有的老年人谢顶,有的老年人不谢顶","谢恩","xiè\u2019ēn","〖thanksb.(esp.theemperororahighofficial)forhisfavor(orkindness)〗感谢别人的恩德(多指臣子君主)","谢候","xièhòu","〖expressappreciation(forsb.\u2019skindnessorhospitality)〗答报致谢并加问候的意思","谢绝","xièjué","〖decline〗婉辞,推辞","谢绝回答有关个人的问题","谢绝一项正式的邀请","谢客","xièkè","〖refusetomeetvisitorsorguests〗∶婉言拒绝会见宾客","〖expressthankstoguests〗∶向宾客致谢","谢礼","xièlǐ","〖honoravium〗向人致谢送的礼物。也叫\u201c谢仪\u201d","谢灵运","XièLíngyùn","〖XieLingyun〗(385\u2014433)南朝宋诗人,曾任永嘉(现在浙江省永嘉县)太守,喜欢游览,擅长写山水诗,有《谢康乐集》","谢幕","xièmù","〖answeracurtaincall〗演出结束后,演员在台前向观众敬礼,表示谢意","谢媒","xièméi","〖(ofbrideandbridegroom)expresstheirthankstothematchmaker〗举行婚礼后男女双方向媒人致谢","谢却","xièquè","〖decline〗谢绝;婉言拒绝","谢世","xièshì","〖passaway;die〗逝世","谢天谢地","xiètiān-xièdì","〖thankgoodness,thankheaven〗迷信说法,谓感谢天地神灵的佑护。后多用来表示满意或感激之意","谢帖","xiètiě","〖anoteofthanks〗旧时受礼后道谢的回帖","谢孝","xièxiào","〖visitandthankrelativesandfriendsfortheirpresenceatfuneral〗旧俗,孝子到吊唁的亲友家行礼致谢,特指服满后拜访吊唁的亲友,表示感谢","谢谢","xièxie","〖thank〗感谢的表示。感谢,尤指对接受别人给予的或提供的恩惠说些感激的话","谢仪","xièyí","〖honorarium〗谢礼。对一般按习惯或规矩不索取固定报酬的服务或在法律上未规定付酬的服务给予的款项或报酬","谢意","xièyì","〖credit〗感谢之意","谢罪","xièzuì","〖apologizeforanoffence〗自认有过,请人谅解","至蔺相如门谢罪。\u2014\u2014《史记·廉颇蔺相如列传》"]
+         */
+
+        private String id;
+        private String zi;
+        private String py;
+        private String wubi;
+        private String pinyin;
+        private String bushou;
+        private String bihua;
+        private List<String> jijie;
+        private List<String> xiangjie;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getZi() {
+            return zi;
+        }
+
+        public void setZi(String zi) {
+            this.zi = zi;
+        }
+
+        public String getPy() {
+            return py;
+        }
+
+        public void setPy(String py) {
+            this.py = py;
+        }
+
+        public String getWubi() {
+            return wubi;
+        }
+
+        public void setWubi(String wubi) {
+            this.wubi = wubi;
+        }
+
+        public String getPinyin() {
+            return pinyin;
+        }
+
+        public void setPinyin(String pinyin) {
+            this.pinyin = pinyin;
+        }
+
+        public String getBushou() {
+            return bushou;
+        }
+
+        public void setBushou(String bushou) {
+            this.bushou = bushou;
+        }
+
+        public String getBihua() {
+            return bihua;
+        }
+
+        public void setBihua(String bihua) {
+            this.bihua = bihua;
+        }
+
+        public List<String> getJijie() {
+            return jijie;
+        }
+
+        public void setJijie(List<String> jijie) {
+            this.jijie = jijie;
+        }
+
+        public List<String> getXiangjie() {
+            return xiangjie;
+        }
+
+        public void setXiangjie(List<String> xiangjie) {
+            this.xiangjie = xiangjie;
+        }
+    }
 }
