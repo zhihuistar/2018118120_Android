0b0f74dec61bd77b20294ec8c47dfc071c65db65
zhihuistar <2602528145@qq.com>
2020-12-17 16:09:30 +0800

    在SearchPinyinActivity类中加载网络数据，添加网络获取失败时需调用的接口

--- Dictionary/app/src/main/java/com/example/dictionary/SearchPinyinActivity.java
+++ Dictionary/app/src/main/java/com/example/dictionary/SearchPinyinActivity.java
@@ -2,11 +2,35 @@ package com.example.dictionary;
 
 import android.os.Bundle;
 
-public class SearchPinyinActivity extends BaseSearchActivity {
+import com.example.dictionary.bean.PinBuWordBean;
+import com.example.dictionary.db.DBManager;
+import com.example.dictionary.utils.CommonUtils;
+import com.example.dictionary.utils.URLUtils;
 
+import java.util.List;
+
+public class SearchPinyinActivity extends BaseSearchActivity {
+    String url;   //获取指定拼音对应的网址
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_pinyin);
+        initData(CommonUtils.FILE_PINYIN,CommonUtils.TYPE_PINYIN);
+        setExLvListener(CommonUtils.TYPE_PINYIN);
+        exLv.expandGroup(0);   //默认展开第一组
+        word = "a";     //默认进去时获取的是第一个 a
+        url = URLUtils.getPinyinurl(word,page,pagesize);
+        // 加载网络数据
+        loadData(url);
+        setGVListener(CommonUtils.TYPE_PINYIN);
+    }
+    /*
+     * 网络获取失败时会调用的接口
+     * 因为拼音查询和部首查询使用的获取数据的方法不一样，所以需要分开写。
+     * 所以就把onError的方法写入到子类当中
+     */
+    @Override
+    public void onError(Throwable ex, boolean isOnCallback) {
+        List<PinBuWordBean.ResultBean.ListBean> list = DBManager.queryPyWordFromPywordtb(word, page, pagesize);
+        refreshDataByGV(list);
     }
 }
