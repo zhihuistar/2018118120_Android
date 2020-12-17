a15b5f374e4bc85bb3f356deb18a16683ff417e6
zhihuistar <2602528145@qq.com>
2020-12-17 10:55:08 +0800

    在CommonUtils类中添加数据库pywordtb表的宏

--- Dictionary/app/src/main/java/com/example/dictionary/utils/CommonUtils.java
+++ Dictionary/app/src/main/java/com/example/dictionary/utils/CommonUtils.java
@@ -5,4 +5,6 @@ public class CommonUtils {
     public static final int TYPE_BUSHOU = 1;
     public static final String FILE_PINYIN = "pinyin.txt";
     public static final String FILE_BUSHOU = "bushou.txt";
+
+    public static final String TABLE_PYWORDTB = "pywordtb";
 }
