68d3dd851be32f311ebf0ba4f178c9d58dfe1c97
zhihuistar <2602528145@qq.com>
2020-12-17 16:04:36 +0800

    在UniteApplication类中初始化数据库对象

--- Dictionary/app/src/main/java/com/example/dictionary/UniteApplication.java
+++ Dictionary/app/src/main/java/com/example/dictionary/UniteApplication.java
@@ -2,11 +2,14 @@ package com.example.dictionary;
 
 import android.app.Application;
 
+import com.example.dictionary.db.DBManager;
+
 import org.xutils.x;
 
 public class UniteApplication extends Application {
     public void onCreate() {
         super.onCreate();
         x.Ext.init(this);   //初始化xUtils的模块数据
+        DBManager.initDB(this);    //初始化数据库对象
     }
 }
