282c6d37db1a6c2b7caf4e4ebf7e8a9c20852eeb
zhihuistar <2602528145@qq.com>
2020-12-18 21:38:19 +0800

    在collect_frag包中新建一个CollectFragmentAdapter类，继承FragmentStatePagerAdapter类

--- Dictionary/app/src/main/java/com/example/dictionary/CollectionActivity.java
+++ Dictionary/app/src/main/java/com/example/dictionary/CollectionActivity.java
@@ -1,14 +1,48 @@
 package com.example.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.dictionary.collect_frag.ZiFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CollectionActivity extends AppCompatActivity {
-
+    TabLayout tabLayout;
+    ViewPager collectVp;
+    String []titles = {"汉字","成语"};
+    List<Fragment> mDatas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
+        tabLayout = findViewById(R.id.collect_tabs);
+        collectVp = findViewById(R.id.collect_vp);
+        initPager();
+    }
+    /* 初始化ViewPager页面的操作*/
+    private void initPager() {
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < titles.length; i++) {
+            Fragment frag = new ZiFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("type",titles[i]);
+            frag.setArguments(bundle);
+            mDatas.add(frag);
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.collect_iv_back:
+                finish();
+                break;
+        }
     }
 }
---
+++ Dictionary/app/src/main/java/com/example/dictionary/collect_frag/CollectFragmentAdapter.java
@@ -0,0 +1,18 @@
+package com.example.dictionary.collect_frag;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class CollectFragmentAdapter extends FragmentStatePagerAdapter {
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
