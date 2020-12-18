622526e79b5822525b846b0f3cf75c07d22e81a0
zhihuistar <2602528145@qq.com>
2020-12-18 20:01:17 +0800

    在CommonUtils类中添加collwordtb表和collcyutb表的宏

--- Dictionary/app/src/main/java/com/example/dictionary/utils/CommonUtils.java
+++ Dictionary/app/src/main/java/com/example/dictionary/utils/CommonUtils.java
@@ -9,4 +9,6 @@ public class CommonUtils {
     public static final String TABLE_PYWORDTB = "pywordtb";
     public static final String TABLE_WORDTB = "wordtb";
     public static final String TABLE_CYUB = "cyutb";
+    public static final String TABLE_COLLECT_WORDTB = "collwordtb";
+    public static final String TABLE_COLLECT_CYUTB = "collcyutb";
 }
