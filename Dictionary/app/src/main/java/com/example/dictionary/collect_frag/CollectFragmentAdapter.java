b467acc79fc8f1358fa321879b2c89d59463625e
zhihuistar <2602528145@qq.com>
2020-12-18 21:40:43 +0800

    在CollectFragmentAdapter类中编写存放Fragment的适配器

--- Dictionary/app/src/main/java/com/example/dictionary/collect_frag/CollectFragmentAdapter.java
+++ Dictionary/app/src/main/java/com/example/dictionary/collect_frag/CollectFragmentAdapter.java
@@ -1,18 +1,31 @@
 package com.example.dictionary.collect_frag;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import java.util.List;
+
 public class CollectFragmentAdapter extends FragmentStatePagerAdapter {
-    @NonNull
+    List<Fragment> list;
+    String[]titles;
+    public CollectFragmentAdapter(FragmentManager fm, List<Fragment> list, String[] titles) {
+        super(fm);
+        this.list = list;
+        this.titles = titles;
+    }
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return list.get(position);
     }
-
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
+    }
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
     }
 }
