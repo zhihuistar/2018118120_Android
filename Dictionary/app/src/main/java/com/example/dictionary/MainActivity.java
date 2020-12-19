bbe1c43d3e31eb448ca5fba06284479df6a22bc8
zhihuistar <2602528145@qq.com>
2020-12-19 10:15:48 +0800

    修改app的icon图标

--- Dictionary/app/src/main/java/com/example/dictionary/MainActivity.java
+++ Dictionary/app/src/main/java/com/example/dictionary/MainActivity.java
@@ -7,7 +7,6 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
